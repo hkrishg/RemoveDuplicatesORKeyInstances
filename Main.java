945cefee406d5f31edda144f514f54241fcfc418
hkrishg <109535284+hkrishg@users.noreply.github.com>
2022-10-11 01:30:57 +0000

    formatted

--- Main.java
+++ Main.java
@@ -1,9 +1,11 @@
 
 class Main {
 
-  // Given an array of sorted numbers, remove all duplicates from it. You should not use any extra space; after removing the duplicates in-place return the new length of the array.
+  // Given an array of sorted numbers, remove all duplicates from it. You should
+  // not use any extra space; after removing the duplicates in-place return the
+  // new length of the array.
 
-    // Input: [2, 3, 3, 3, 6, 9, 9]
+  // Input: [2, 3, 3, 3, 6, 9, 9]
   // Output: 4
   // Explanation: The first four elements after removing the duplicates will be
   // [2, 3, 6, 9].
@@ -20,12 +22,12 @@ class Main {
     return nonDuplicateIndex;
   }
 
-  // Problem 1: Given an unsorted array of numbers and a target ‘key’, remove all instances of ‘key’ in-place and return the new length of the array.
-    // Input: [3, 2, 3, 6, 3, 10, 9, 3], Key=3
+  // Problem 1: Given an unsorted array of numbers and a target ‘key’, remove all
+  // instances of ‘key’ in-place and return the new length of the array.
+  // Input: [3, 2, 3, 6, 3, 10, 9, 3], Key=3
   // Output: 4
   // Explanation: The first four elements after removing every 'Key' will be [2,
   // 6, 10, 9].
-
 
   private static int removeKeyInstances(int[] arr, int key) {
     int nonDuplicateIndex = 0;
@@ -38,9 +40,6 @@ class Main {
     }
     return nonDuplicateIndex;
   }
-
-
-
 
   public static void main(String[] args) {
     int result = Main.lengthOfNonDuplicates(new int[] { 2, 3, 3, 3, 6, 9, 9 });
